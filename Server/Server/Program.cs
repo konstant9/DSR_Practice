ee1cbd21f19e8114d594de51fbf7a0cf7bc3d3da
konstant9 <konstant9@yandex.ru>
2015-07-13 11:48:01 +0300

    Add Project

---
+++ Server/Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var listener = new ModifiedHttpListener(new Uri("http://localhost:8080/"));
+            listener.StartListen();
+
+            Console.WriteLine("Listening...");
+            Console.Read();
+
+        }
+    }
+}
